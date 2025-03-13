60edb0245cf7862b76e317360e846fdb1e6149e9
Aaron Stanek <amr.stanek@gmail.com>
2025-03-13 11:02:30 -0700

    send explicit HTTP response on success

--- py/web_server.py
+++ py/web_server.py
@@ -8,6 +8,10 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         try:
             submit_best_weather(weathervane())
             self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-Length", "2")
+            self.end_headers()
+            self.wfile.write(b"OK")
         except:
             self.send_response(500)
 
